7f6bfb62c82194363908ee595493ab340a6544aa
Quentin Tarantino <69848148+OJS9376@users.noreply.github.com>
2026-09-22 10:21:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -276,7 +276,7 @@ if df_sched is not None and df_check is not None:
                     df_to_save = pd.DataFrame(records)
                     st.session_state.df_cal_data = df_to_save
                     
-                    API_URL = "https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=xlsx"
+                    API_URL = "https://script.google.com/macros/s/AKfycbw_tlpScpdqeBAaVvsE1856f31cpiaKJg4ik38Hm-70s_qvyZJRwDb0k9HVhSaZDfgh/exec"
                     try:
                         requests.post(API_URL, json=df_to_save.to_dict(orient="records"), timeout=5)
                         st.success("구글 스프레드시트에 품질활동 일정이 영구 저장되었습니다.")
@@ -305,7 +305,7 @@ if df_sched is not None and df_check is not None:
                 df_main_save = pd.DataFrame(records_main)
                 st.session_state.df_cal_data = df_main_save
                 
-                API_URL = "https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=xlsx"
+                API_URL = "https://script.google.com/macros/s/AKfycbw_tlpScpdqeBAaVvsE1856f31cpiaKJg4ik38Hm-70s_qvyZJRwDb0k9HVhSaZDfgh/exec"
                 try:
                     requests.post(API_URL, json=df_main_save.to_dict(orient="records"), timeout=5)
                 except:
