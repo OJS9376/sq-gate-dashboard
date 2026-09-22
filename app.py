d021a8ac6f8a4fff564fc518e17add64449d7957
Quentin Tarantino <69848148+OJS9376@users.noreply.github.com>
2026-09-22 10:08:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ if df_sched is not None and df_check is not None:
                             for t_val, e_val in d_evs.items():
                                 if e_val.strip():
                                     is_done_btn = st.session_state.get(f"cal_status_{st.session_state.cal_year}_{m_idx}_{d_idx}_{t_val}", False)
-                                    records.append({"Year": st.session_state.cal_year, "Month": m_idx, "Day": d_idx, "Time": t_val, "Event": e_val, "Is_Done": str(is_done_btn)})
+                                   records.append({"Year": int(st.session_state.cal_year),"Month": int(m_idx),"Day": int(d_idx),"Time": str(t_val),"Event": str(e_val),"Is_Done": str(is_done_btn)})
                     
                     df_to_save = pd.DataFrame(records)
                     st.session_state.df_cal_data = df_to_save
